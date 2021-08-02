169f100b1f8ff3a5d4f202583411a031b6f04387
Sadaqat <izahid521@yahoo.com>
2021-08-03 03:21:29 +0500

    //arbitrary Arguments Example

---
+++ main.py
@@ -0,0 +1,9 @@
+
+
+def my_function(*kids):
+    print("The first child is "+ kids[0])
+    print("The second child is " + kids[1])
+    print("The Third child is " + kids[2])
+   # print("The fourth child is " + kids[3])
+
+my_function("Emil", "Tobias", "Linus")
